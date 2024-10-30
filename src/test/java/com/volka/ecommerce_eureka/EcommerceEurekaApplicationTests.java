d25a55fb37de95620080cf06ddaf7ee5985779f1
volka <volka5091@gmail.com>
2024-10-30 22:50:35 +0900

    [INIT] 유레카 초기 설정

---
+++ src/test/java/com/volka/ecommerce_eureka/EcommerceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volka.ecommerce_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
