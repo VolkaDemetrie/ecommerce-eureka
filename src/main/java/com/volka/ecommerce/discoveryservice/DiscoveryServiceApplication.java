97236cf194ae9c4b87adb0ac17e093f3ea31fe76
volka <volka5091@gmail.com>
2024-10-30 23:20:14 +0900

    [INIT] 유레카 초기 설정

--- src/main/java/com/volka/ecommerce/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/volka/ecommerce/discoveryservice/DiscoveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //유레카 서버로 사용
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
